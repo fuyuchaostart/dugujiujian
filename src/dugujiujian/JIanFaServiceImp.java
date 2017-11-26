d5f15e384f06ddfb1209c3d05599f6e7e9b8fc3a
fycstart <fycstart@126.com>
2017-11-26 13:14:07 +0800

    褚禄山

--- src/dugujiujian/JIanFaServiceImp.java
+++ src/dugujiujian/JIanFaServiceImp.java
@@ -11,7 +11,7 @@ public class JIanFaServiceImp implements DuGuJiuJIangService {
 		System.out.println("三过天门而不入--吕洞玄");
 		System.out.println("我有一剑可斩仙人-徐凤年");
 		System.out.println("官子无敌,曹青衣-曹风流");
-		System.out.println("千骑开蜀--褚八叉");
+		System.out.println("千骑开蜀--褚八叉 ");
 	}
 
 }
