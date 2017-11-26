ea041bcd3fe066b588825ba14ac599704dc8fe39
fycstart <fycstart@126.com>
2017-11-26 10:51:06 +0800

    剑法实现类

---
+++ src/dugujiujian/JIanFaServiceImp.java
@@ -0,0 +1,10 @@
+package dugujiujian;
+
+public class JIanFaServiceImp implements DuGuJiuJIangService {
+
+	@Override
+	public void getJianFa(String str) {
+		System.out.println("“ªΩ£œ…»ÀπÚ");
+	}
+
+}
