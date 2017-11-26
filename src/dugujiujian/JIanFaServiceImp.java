dc74532cab84cc43eb31d2862a8c78f4d6b0ab28
fycstart <fycstart@126.com>
2017-11-26 11:16:43 +0800

    薛宋官

--- src/dugujiujian/JIanFaServiceImp.java
+++ src/dugujiujian/JIanFaServiceImp.java
@@ -6,6 +6,7 @@ public class JIanFaServiceImp implements DuGuJiuJIangService {
 	public void getJianFa(String str) {
 		System.out.println("一剑仙人跪--李纯刚");
 		System.out.println("剑开天门--李纯刚");
+		System.out.println("胡笳十八拍--薛宋官");
 	}
 
 }
